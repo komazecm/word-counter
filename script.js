c784d51b033e7222313d340fb37cbbc98d64fec5
Masa Komazec <masakomazec9@gmail.com>
2021-11-25 20:15:58 +0100

    fixed errors

--- script.js
+++ script.js
@@ -9,23 +9,43 @@ btn.addEventListener('click', countWords);
 
 //functions:
 function countWords() {
-    //counting words:
-    let words = inputText.value.split(" ");    
-    let counter = removeEmptyStrings(words);
     //counting characters:
     let char = inputText.value.split("");
-    
-    wordNo.innerText = words.length - counter + " words \n " + char.length + " characters";
+
+    let words = replaceNewLineChar(char);
+    words = words.toString();
+    console.log(words)
+    words = words.split(" ");
+    words.forEach(element => {
+        console.log(element)
+    });
+
+    let [counter, newLineCounter] = removeEmptyStrings(words);
+  
+    wordNo.innerText = words.length - counter - newLineCounter + " words \n " + char.length + " characters";
 
     console.log(words.length-counter);
 }
 
 function removeEmptyStrings(array) {
     let counter = 0;
+    let newLineCounter = 0;
     array.forEach(element => {
         if(element === "") {
             counter++;
         }
+        if(element===",") {
+            newLineCounter++;
+        }
     });
-    return counter;
+    return [counter, newLineCounter];
+}
+
+function replaceNewLineChar(array) {
+    for (let index = 0; index < array.length; index++) {
+       if (array[index]==="\n") {
+        array.splice(index, 1, " ");
+       }
+    }
+    return array;
 }
